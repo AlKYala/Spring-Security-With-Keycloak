abc9ec5f39f50f75dbe93594dc6981bd357563a8
Ali Yalama <aliyalama@googlemail.com>
2023-03-06 14:39:03 +0100

    Initial commit

---
+++ src/test/java/de/ayalama/springsecuritywithkeycloak/SpringSecurityWithKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ayalama.springsecuritywithkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
